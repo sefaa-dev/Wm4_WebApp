c3b956581f57b2cb8d7c054e1bef23963bd1ad57
sefaa-dev <sefayilmaz582@gmail.com>
2021-12-24 14:12:01 +0300

    fdg

---
+++ ItServiceApp/Models/Sevices/EmailService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItServiceApp.Models.Sevices
+{
+    public class EmailSender
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmailSender(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string SenderMail => _configuration.GetSection("EmailOptions.SenderMail").Value;
+        public string Password => _configuration.GetSection("EmailOptions.Password").Value;
+        public string Smtp => _configuration.GetSection("EmailOptions.Smtp").Value;
+        public int SmtpPort => Convert.ToInt32(_configuration.GetSection("EmailOptions.SmtpPort").Value);
+    }
+}
