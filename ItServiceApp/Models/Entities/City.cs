13ce247c0256bd777134504fef125cf0b7b76efd
sefaa-dev <sefayilmaz582@gmail.com>
2022-01-12 11:19:18 +0300

    ghh

---
+++ ItServiceApp/Models/Entities/City.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItServiceApp.Models.Entities
+{
+    public class City
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+
+        public virtual List<State> States { get; set; }
+    }
+
+    public class State
+    {
+        [Key]
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CityId { get; set; }
+
+        [ForeignKey("CityId")]
+        public virtual City City { get; set; }
+
+    }
+}
