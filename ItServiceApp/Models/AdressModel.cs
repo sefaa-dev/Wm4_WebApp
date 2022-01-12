0ca26a20d2646747d5d75c6fb4c7def15a6ed890
sefaa-dev <sefayilmaz582@gmail.com>
2022-01-12 13:50:33 +0300

    hjkh

---
+++ ItServiceApp/Models/AdressModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItServiceApp.Models
+{
+    public class AdressModel
+    {
+        public string Description { get; set; }
+        public string ZipCode { get; set; }
+        public string ContactName { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
