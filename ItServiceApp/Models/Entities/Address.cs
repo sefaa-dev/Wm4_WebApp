7d29ba5e6061a3091a870d27514b4deb02b2ac21
sefaa-dev <sefayilmaz582@gmail.com>
2022-01-12 11:12:19 +0300

    ghj

---
+++ ItServiceApp/Models/Entities/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItServiceApp.Models.Entities
+{
+    public class Address : BaseEntity
+    {
+        public string Line { get; set; }
+        public string PostCode { get; set; }
+        public AddressTypes AddressType { get; set; }
+    }
+
+    public enum AddressTypes
+    {
+        Fatura,
+        Teslimat
+    }
+}
---
+++ ItServiceApp/Models/Entities/Subscription.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItServiceApp.Models.Entities
+{
+    public class Subscription : BaseEntity
+    {
+        public Guid SubscriptionTypeId { get; set; }
+        public decimal Amount { get; set; }
+        public decimal PaidAmount { get; set; }
+        public DateTime EndDate { get; set; }
+
+        [NotMapped]
+        public bool IsActive => EndDate > DateTime.Now;
+        
+        [ForeignKey(nameof(SubscriptionTypeId))]
+        public virtual SubscriptionType SubscriptionType { get; set; }
+    }
+
+
+    public class SubscriptionType : BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Month { get; set; }
+    }
+}
