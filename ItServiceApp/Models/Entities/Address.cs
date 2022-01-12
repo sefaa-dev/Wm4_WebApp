ddd3790cb9418df0d03287f0254155f71f39810e
sefaa-dev <sefayilmaz582@gmail.com>
2022-01-12 11:30:37 +0300

    yıy

--- ItServiceApp/Models/Entities/Address.cs
+++ ItServiceApp/Models/Entities/Address.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ItServiceApp.Models.Identity;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +13,21 @@ namespace ItServiceApp.Models.Entities
         public string Line { get; set; }
         public string PostCode { get; set; }
         public AddressTypes AddressType { get; set; }
+        public int CityId { get; set; }
+        public int StateId { get; set; }
+
+
+        [StringLength(128)]
+        public string UserId { get; set; }
+
+        [ForeignKey(nameof(CityId))]
+        public virtual City City { get; set; }
+
+        [ForeignKey(nameof(StateId))]
+        public virtual State State { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public virtual ApplicationUser User { get; set; }
     }
 
     public enum AddressTypes
--- ItServiceApp/Models/Identity/ApplicationUser.cs
+++ ItServiceApp/Models/Identity/ApplicationUser.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using ItServiceApp.Models.Entities;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -14,5 +15,7 @@ namespace ItServiceApp.Models.Identity
         [StringLength(50)]
         public string SurName { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public virtual List<Address> Addresses { get; set; }
+        public virtual List<Subscription> Subscriptions { get; set; }
     }
 }
