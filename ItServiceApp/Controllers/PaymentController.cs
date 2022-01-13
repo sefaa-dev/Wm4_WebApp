360a7db39f8901cdef4da4b34a2155de9db719ce
sefaa-dev <sefayilmaz582@gmail.com>
2022-01-13 09:08:44 +0300

    hjg

---
+++ ItServiceApp/Controllers/PaymentController.cs
@@ -0,0 +1,25 @@
+﻿using ItServiceApp.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItServiceApp.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly IPaymentService _paymentService;
+        public PaymentController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
