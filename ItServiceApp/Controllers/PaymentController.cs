d4f0a4107a3e232c1b6d0bc6b9aeeac53db5e6ae
sefaa-dev <sefayilmaz582@gmail.com>
2022-01-14 12:18:22 +0300

    şlkjhgfds

--- ItServiceApp/Controllers/PaymentController.cs
+++ ItServiceApp/Controllers/PaymentController.cs
@@ -6,7 +6,7 @@ using ItServiceApp.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
+using System.Collections.Generic; 
 using System.Linq;
 using System.Threading.Tasks;
 
