0db2dd2730ce61644a3105ab3868ab76b600ef6b
sefaa-dev <sefayilmaz582@gmail.com>
2022-01-17 13:54:21 +0300

    gsfd

--- ItServiceApp/Areas/Admin/Controllers/ManageController.cs
+++ ItServiceApp/Areas/Admin/Controllers/ManageController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;  
+using System.Threading.Tasks;   
 
 namespace ItServiceApp.Areas.Admin.Controllers  
 {
