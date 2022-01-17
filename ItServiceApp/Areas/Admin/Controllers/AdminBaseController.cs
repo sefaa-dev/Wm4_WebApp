568d56d246ac507c9f618c85d08302181b161594
sefaa-dev <sefayilmaz582@gmail.com>
2022-01-17 11:51:38 +0300

    hjgfdc

---
+++ ItServiceApp/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItServiceApp.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminBaseController : Controller
+    {
+       
+    }
+}
