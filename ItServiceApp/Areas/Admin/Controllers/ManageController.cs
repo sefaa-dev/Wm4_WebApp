ed4fd318b5236b2c481e81edf5ee0afa97088373
sefaa-dev <sefayilmaz582@gmail.com>
2022-01-17 13:37:57 +0300

    jkhgfd

--- ItServiceApp/Areas/Admin/Controllers/ManageController.cs
+++ ItServiceApp/Areas/Admin/Controllers/ManageController.cs
@@ -12,5 +12,10 @@ namespace ItServiceApp.Areas.Admin.Controllers
         {
             return View();
         }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
     }
 }
