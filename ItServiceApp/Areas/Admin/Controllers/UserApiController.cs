11c088d085ba45ad64f186e041210f81c4f4111a
sefaa-dev <sefayilmaz582@gmail.com>
2022-01-18 09:44:48 +0300

    şlkj

---
+++ ItServiceApp/Areas/Admin/Controllers/UserApiController.cs
@@ -0,0 +1,33 @@
+﻿using ItServiceApp.Models.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItServiceApp.Areas.Admin.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class UserApiController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserApiController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = _userManager.Users.OrderBy(x => x.CreatedDate).ToList();
+
+            return Ok(users);
+        }
+    }
+}
