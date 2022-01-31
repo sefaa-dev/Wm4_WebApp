c4b7f2197d9c0d0a8b5925b976a212d43a33824e
sefaa-dev <sefayilmaz582@gmail.com>
2022-01-31 15:10:27 +0300

    lıukjyt

--- ItServiceApp/Controllers/HomeController.cs
+++ ItServiceApp/Controllers/HomeController.cs
@@ -27,6 +27,7 @@ namespace ItServiceApp.Controllers
 
             _myDependency.Log("Home/Index'e girildi");
             var data = _dbContext.SubscriptionTypes
+                .ToList()
                 .Select(x => _mapper.Map<SubscriptionTypeViewModel>(x))
                 .OrderBy(x => x.Price)
                 .ToList();
