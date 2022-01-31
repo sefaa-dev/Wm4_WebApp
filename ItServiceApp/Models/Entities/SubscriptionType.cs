47776d93ed58fc7c4dbc9eafc1d0480f0464235d
sefaa-dev <sefayilmaz582@gmail.com>
2022-01-31 11:06:39 +0300

    7o8ıut6yt

--- ItServiceApp/Models/Entities/SubscriptionType.cs
+++ ItServiceApp/Models/Entities/SubscriptionType.cs
@@ -10,5 +10,6 @@ namespace ItServiceApp.Models.Entities
         public string Description { get; set; }
 
         public int Month { get; set; }
+        public decimal Price { get; set; }
     }
 }
