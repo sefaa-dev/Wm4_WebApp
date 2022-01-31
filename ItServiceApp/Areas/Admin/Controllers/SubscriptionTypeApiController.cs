a78892c573fcd95e8035e2d6fe31120d5e0cc8fe
sefaa-dev <sefayilmaz582@gmail.com>
2022-01-31 13:07:03 +0300

    ıukjyhtgrdfsd

--- ItServiceApp/Areas/Admin/Controllers/SubscriptionTypeApiController.cs
+++ ItServiceApp/Areas/Admin/Controllers/SubscriptionTypeApiController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
+using System.Collections.Generic; 
 using System.Linq;
 using System.Threading.Tasks;
 
