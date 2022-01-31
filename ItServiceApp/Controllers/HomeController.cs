40aec13801b578cd0f601c3de67c5f1aca234cc7
sefaa-dev <sefayilmaz582@gmail.com>
2022-01-31 14:56:09 +0300

    8675

--- ItServiceApp/Controllers/HomeController.cs
+++ ItServiceApp/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using ItServiceApp.Data;
+﻿using AutoMapper;
+using ItServiceApp.Data;
 using ItServiceApp.InjectOrnek;
+using ItServiceApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -12,18 +14,24 @@ namespace ItServiceApp.Controllers
     {
         private readonly IMyDependency _myDependency;
         private readonly MyContext _dbContext;
-        public HomeController(IMyDependency myDependency, MyContext dbContext)
+        private readonly IMapper _mapper;
+        public HomeController(IMyDependency myDependency, MyContext dbContext, IMapper mapper)
         {
             _myDependency = myDependency;
             _dbContext = dbContext;
+            _mapper = mapper;
         }
         
         public IActionResult Index()
         {
 
             _myDependency.Log("Home/Index'e girildi");
-            var data = _dbContext.SubscriptionTypes.ToList();
-            return View();
+            var data = _dbContext.SubscriptionTypes
+                .Select(x => _mapper.Map<SubscriptionTypeViewModel>(x))
+                .OrderBy(x => x.Price)
+                .ToList();
+
+            return View(data);
         }
     }
 }
