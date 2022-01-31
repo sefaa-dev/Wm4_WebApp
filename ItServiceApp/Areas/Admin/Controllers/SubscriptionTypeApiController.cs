1e89bd42ad5c9291d8da449b4a904dc3bd98200c
sefaa-dev <sefayilmaz582@gmail.com>
2022-01-31 11:56:11 +0300

    ıukhjyt

---
+++ ItServiceApp/Areas/Admin/Controllers/SubscriptionTypeApiController.cs
@@ -0,0 +1,112 @@
+﻿using DevExtreme.AspNet.Data;
+using ItServiceApp.Data;
+using ItServiceApp.Extensions;
+using ItServiceApp.Models.Entities;
+using ItServiceApp.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItServiceApp.Areas.Admin.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class SubscriptionTypeApiController : Controller
+    {
+        private readonly MyContext _dbContext;
+
+        public SubscriptionTypeApiController(MyContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        #region Cruds
+        [HttpGet]
+        public IActionResult Get(DataSourceLoadOptions options)
+        {
+            var data = _dbContext.SubscriptionTypes;
+                
+
+            return Ok(DataSourceLoader.Load(data, options));
+        }
+        [HttpGet]
+        public IActionResult Detail(Guid id, DataSourceLoadOptions loadOptions)
+        {
+            var data = _dbContext.SubscriptionTypes
+                .Where(x => x.Id == id);
+
+            return Ok(DataSourceLoader.Load(data, loadOptions));
+        }
+        [HttpPost]
+        public IActionResult Insert(string values)
+        {
+            var data = new SubscriptionType();
+            JsonConvert.PopulateObject(values, data);
+
+            if (!TryValidateModel(data))
+                return BadRequest(new JsonResponseViewModel()
+                {
+                    IsSuccess = false,
+                    ErrorMessage = ModelState.ToFullErrorString()
+                });
+            _dbContext.SubscriptionTypes.Add(data);
+
+            var result = _dbContext.SaveChanges();
+            if (result == 0)
+                return BadRequest(new JsonResponseViewModel
+                {
+                    IsSuccess = false,
+                    ErrorMessage = "Yeni üyelik tipi kaydedilemedi."
+                });
+            return Ok(new JsonResponseViewModel());
+        }
+        [HttpPut]
+        public IActionResult Update(Guid key, string values)
+        {
+            var data = _dbContext.SubscriptionTypes.Find(key);
+            
+            if (data == null)
+                return BadRequest(new JsonResponseViewModel()
+                {
+                    IsSuccess = false,
+                    ErrorMessage = ModelState.ToFullErrorString()
+                });
+
+            JsonConvert.PopulateObject(values, data);
+            if (!TryValidateModel(data))
+                return BadRequest(ModelState.ToFullErrorString());
+
+            var result = _dbContext.SaveChanges();
+            if (result == 0)
+                return BadRequest(new JsonResponseViewModel()
+                {
+                    IsSuccess = false,
+                    ErrorMessage = "Üyelik tipi güncellenemedi"
+                });
+            return Ok(new JsonResponseViewModel());
+        }
+        [HttpDelete]
+        public IActionResult Delete(Guid key)
+        {
+            var data = _dbContext.SubscriptionTypes.Find(key);
+            if (data == null)
+                return StatusCode(StatusCodes.Status409Conflict, "Üyelik tipi bulunamadı");
+
+            _dbContext.SubscriptionTypes.Remove(data);
+
+            var result = _dbContext.SaveChanges();
+            if (result == 0)
+                return BadRequest("Silme işlemi başarısız");
+            return Ok(new JsonResponseViewModel());
+        }
+        #endregion
+
+
+
+
+    }
+}
+
