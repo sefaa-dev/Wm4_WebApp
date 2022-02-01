b024a3cebb6c0e4927af3ef498c12870d06f23a8
sefaa-dev <sefayilmaz582@gmail.com>
2022-02-01 13:25:37 +0300

    jfdsa

--- ItServiceApp/ViewModels/SubscriptionTypeViewModel.cs
+++ ItServiceApp/ViewModels/SubscriptionTypeViewModel.cs
@@ -7,6 +7,7 @@ namespace ItServiceApp.ViewModels
 {
     public class SubscriptionTypeViewModel
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public int Month { get; set; }
